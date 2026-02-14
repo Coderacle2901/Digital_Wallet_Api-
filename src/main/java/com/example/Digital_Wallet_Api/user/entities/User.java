55816f96e9fcc19ce1da21338e73cd6349573ae6
Miracle Rapheal <miraclerapheal14@gmail.com>
2026-02-14 07:02:33 +0100

    "feat: added User entity with fields, enums, and timestamps"

---
+++ src/main/java/com/example/Digital_Wallet_Api/user/entities/User.java
@@ -0,0 +1,53 @@
+package com.example.Digital_Wallet_Api.user.entities;
+
+import com.example.Digital_Wallet_Api.user.entities.enums.Role;
+import com.example.Digital_Wallet_Api.user.entities.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/example/Digital_Wallet_Api/user/entities/enums/Status.java
@@ -0,0 +1,5 @@
+package com.example.Digital_Wallet_Api.user.entities.enums;
+
+public enum Status {
+    ACTIVE,INACTIVE,BLOCKED;
+}
