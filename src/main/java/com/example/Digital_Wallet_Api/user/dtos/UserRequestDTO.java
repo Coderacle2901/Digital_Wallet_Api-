231139fafbc25705b8a08684a6d41d16e3ef4cf7
Miracle Rapheal <miraclerapheal14@gmail.com>
2026-02-14 10:05:04 +0100

    "feat: added fields and validations to user request and response dtos"

--- src/main/java/com/example/Digital_Wallet_Api/user/dtos/UserRequestDTO.java
+++ src/main/java/com/example/Digital_Wallet_Api/user/dtos/UserRequestDTO.java
@@ -1,5 +1,25 @@
 package com.example.Digital_Wallet_Api.user.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
 public class UserRequestDTO {
 
+    @NotBlank(message = "FirstName cannot be empty")
+    private String firstName;
+
+    @NotBlank(message = "LastName cannot be empty")
+    private String lastName;
+
+    @NotBlank(message = "email is required")
+    @Email(message = "Please enter a valid email address, example: example@gmail.com")
+    private String email;
+
+    @NotBlank(message = "password is required")
+   @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$",
+           message = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character")
+    private String password;
 }
--- src/main/java/com/example/Digital_Wallet_Api/user/dtos/UserResponseDTO.java
+++ src/main/java/com/example/Digital_Wallet_Api/user/dtos/UserResponseDTO.java
@@ -1,4 +1,19 @@
 package com.example.Digital_Wallet_Api.user.dtos;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class UserResponseDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
